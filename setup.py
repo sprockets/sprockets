f41c6cb36a42620f37492afa77668b7fbe577b6e
Gavin M. Roy <gavinr@aweber.com>
2014-08-29 09:52:01 -0400

    Add namespaced_packages entry

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 import sys
 
 requirements = []
-tests_require = ['coverage', 'coveralls', 'mock', 'nose']
+tests_require = ['coverage', 'coveralls', 'nose']
 
 # Requirements for Python 2.6
-version = sys.version_info
-if (version.major, version.minor) < (2, 7):
+if sys.version_info < (2, 7):
     requirements.append('argparse')
     requirements.append('importlib')
     requirements.append('logutils')
     tests_require.append('unittest2')
+if sys.version_info < (3, 0):
+    tests_require.append('mock')
+
 
 setup(name='sprockets',
       version='0.1.0',
@@ -22,27 +24,29 @@ setup(name='sprockets',
       url='https://github.com/sprockets/sprockets',
       install_requires=requirements,
       license=open('LICENSE').read(),
+      namespace_packages=['sprockets'],
       package_data={'': ['LICENSE', 'README.rst']},
       packages=['sprockets'],
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Environment :: No Input/Output (Daemon)',
-                   'Framework :: Tornado',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Natural Language :: English',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.2',
-                   'Programming Language :: Python :: 3.3',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: Implementation :: CPython',
-                   'Programming Language :: Python :: Implementation :: PyPy',
-                   'Topic :: Internet :: WWW/HTTP',
-                   'Topic :: Software Development :: Libraries',
-                   'Topic :: Software Development :: Libraries :: Python Modules'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: No Input/Output (Daemon)',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
       test_suite='nose.collector',
       tests_require=tests_require,
       zip_safe=False)
